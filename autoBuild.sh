9dd706cc09e133de5202d8814afaf42f3d1bea25
Tim <timchenut567@utexas.org>
2023-09-10 16:53:43 -0500

    delete shell script (causes issues with signal handlers, shell script gets stopped by ctrlZ)

--- autoBuild.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#Script needs permissions to run, given by "chmod +x autoBuild.sh". Then build by running "./autoBuild.sh"
-make
-./yash 
-
-#ISSUE: calling ctrl+Z interrupts this bash script, wont work with that interrupt
